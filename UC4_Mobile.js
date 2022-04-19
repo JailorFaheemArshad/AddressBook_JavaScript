f1b4b9ec31656c08bad1cefd2740ef8ac667dbec
faheem <lalafahim55@gmail.com>
2022-04-19 23:36:06 +0530

    Faheem for regex

---
+++ UC4_Mobile.js
@@ -0,0 +1,13 @@
+
+const regex = /^[1-9]{1,2}[ ][1-9]{1}[0-9]{9}$/;
+let str = "91 9897139203";
+
+let result = regex.exec(str);
+console.log("The result from exec is : ", result);
+
+if(regex.test(str)){
+    console.log(`The string ${str} matchs the expression ${regex.source}`);
+}
+else{
+    console.log(`The string ${str} does not match the expression ${regex.source}`);
+}
